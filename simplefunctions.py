0430067167a9dab67c9ecc9e87b4f071679f500e
Damian Giza <damian.giza1@gmail.com>
2020-12-29 00:18:30 +0100

    3 new functions

--- simplefunctions.py
+++ simplefunctions.py
@@ -1,4 +1,4 @@
-def factorial(number):                  # silnia
+def factorial(number):                      # silnia
     factorial = number
     number = number - 1
     while number > 0:
@@ -6,13 +6,13 @@ def factorial(number):                  # silnia
         number = number -1
     return factorial
 
-def string_rev(str1):                   # odwróć strin
+def string_rev(str1):                       # odwróć strin
     r_str1 = ''
     index = len(str1)
     while index > 0:
-        rstr1 += str1[ index - 1]
+        r_str1 += str1[ index - 1]
         index = index -1
-    return rstr1
+    return r_str1
 
 def in_range(number, ranged, rangeu):       # czy liczba mieści się w zakresie
     if number in range(ranged, rangeu):
@@ -37,7 +37,7 @@ def unique_elements_list(list1):            # zwraca tylko unikalne rekordy z li
             list2.append(i)
     return list2    
 
-def is_prime(number):
+def is_prime(number):                       # sprawdza czy liczba pierwsza
     prime = ''
     if number==1:
         return 'Number is not prime'
@@ -48,9 +48,53 @@ def is_prime(number):
         if prime == '':
             prime = 'Number is prime'
         return prime
+
+def even_nums(list1):                       # zwraca liczby parzyste
+    list2 = []
+    for n in list1:
+        if (n%2) == 0:
+            list2.append(n)
+    return list2
+
+def isperfect_num(num):                       # sprawdza czy perfect number - numer którego suma dzielników jest równa temu numerowi
+    divsum = 0
+    for n in range(1,num):
+        if (num%n) == 0:
+            divsum += n
+    if divsum == num:
+        print('Number is perfect')
+    else:
+        print('Number is not perfect')
+
+
+def ispalindrome(string):
+    left = 0
+    right = len(string) - 1
+
+    while right >= left:
+        if not string[left] == string[right]:
+            return False
+        left += 1
+        right -=1
+    return True
+
+def pascal_triangle(rows):
+    print('1')
+    print('1 1')
+    list1 = [1,1]
+    for i in range(2,rows):
+        list2 = []
+        for n in range(0, (len(list1)-1)):
+            list2.append(list1[n]+list1[n+1])
+            list1.insert(n+1, list2[n])
+        print(list1)
+
+
         
+print(pascal_triangle(4))
+#print(ispalindrome('nursesrun'))
 
-     
-
-print(is_prime(1))
+# print(perfect_num(496))
+# print(even_nums([1,2,3,4,5,6,7]))
+# print(is_prime(1))
 # print(count_upandlowletters('Dam ianGizA '))
